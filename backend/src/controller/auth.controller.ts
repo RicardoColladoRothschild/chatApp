87c0a1dc245d941f6471a5047dd045a39a8768ed
Ricardo Collado Rothschild <ricardocolladorothschild@gmail.com>
2025-06-09 13:29:23 -0400

    complete some validation for signup

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -6,8 +6,47 @@ export function AuthenticationController():AuthenticationControllable{
 
     return{
         async signUp(req: Request, res: Response): Promise<void>{
-        console.info("[X] - api/auth: Signing up");
-         res.send("Signing up");
+        console.info("[X] - Signing up");
+            const {email, password, fullname} = req.body;
+           
+            try{
+                 if(!email || !password || !fullname){
+                    console.info("[X] - Must provide all require fields.");
+
+                    res
+                      .status(400)
+                      .json({errorMessage:"Missing information"});
+                }
+                
+                if(password.length < 6){
+                    console.info("[X] - Invalid password length error message send.");
+
+                    res
+                        .status(400)
+                        .json({errorMessage:"Password must be more than 6 characters"});
+                }
+
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,50}$/;
+
+                if(!emailRegex.test(email)){
+                    console.info("[X] - Invalid email error message send.");
+
+                    res.status(400)
+                        .json({errorMessage:`"${email}" is not a valid email address`});
+                }
+
+                if(nameRegex.test(fullname)){
+                    console.info("[X] - Invalid fullname error message send.");
+                    res
+                        .status(400)
+                        .json({errorMessage:`"${fullname}" is not a valid name`});
+                }
+            }catch(error){
+                res
+                    .status(500)
+                    .json({errorCode:500, message:`Unable to signup: ${error}`});
+            }
         },
         
         async login(req:Request, res: Response){
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,8 +11,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 
-
-
+app.use(express.json());
+// app.get("/", (req, res)=>{
+//     res.send("HelloWord");
+// });
 
 app.use("/api/auth", authRoutes);
 
